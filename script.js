98b51cb2aaac72f2079599e94d900dea7e040683
imranomb <imran2017omerbasic@hotmail.com>
2024-03-03 13:39:07 +0100

    2.0

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let cards = document.querySelectorAll(".card");
 let sections = document.querySelectorAll(".section");
 let ths = 1;
-
+let transitionDelay = 0.3;
 thresholdSetter();
 
 function thresholdSetter()
 {
-    if(screen.width > 767) ths = 1;
-    else ths = 0;
+    if(screen.width > 767) ths = 0.6;
+    else ths = 0.6;
 }
 
 const observer = new IntersectionObserver((obj) => {
@@ -18,9 +18,12 @@ const observer = new IntersectionObserver((obj) => {
             document.getElementById("naslov2").classList.toggle("turnOn");
             document.getElementById("wave").classList.toggle("turnOn");
             cards.forEach(card => {
+                card.style.transitionDelay = `${transitionDelay}s`
                 card.classList.toggle("turnOn")
+                transitionDelay = transitionDelay + 0.3;
+                console.log("je")
             })
-            document.getElementById("slider").classList.toggle("turnOn")
+            // document.getElementById("slider").classList.toggle("turnOn")
             observer.unobserve(entry.target);
         }
         else if(entry.target.getAttribute("id") == "reservation" && entry.isIntersecting)
@@ -47,69 +50,69 @@ const observer = new IntersectionObserver((obj) => {
 }
 )
 
+
 sections.forEach(section => {
     observer.observe(section);
 })
 
 
+// const tabsBox = document.querySelector(".usluge"),
+// allTabs = tabsBox.querySelectorAll(".card")
 
-const tabsBox = document.querySelector(".usluge"),
-allTabs = tabsBox.querySelectorAll(".card")
+// let isDragging = false;
+// let isHover = false;
+// let slidePath = 0;
+// let slideGap = 40;
 
-let isDragging = false;
-let isHover = false;
-let slidePath = 0;
-let slideGap = 40;
+// const dragging = (e) => {
+//     if(!isDragging) return;
+//     tabsBox.classList.add("dragging");
+//     tabsBox.scrollLeft -= e.movementX;
+// }
 
-const dragging = (e) => {
-    if(!isDragging) return;
-    tabsBox.classList.add("dragging");
-    tabsBox.scrollLeft -= e.movementX;
-}
+// const dragStop = () => {
+//     isDragging = false;
+//     tabsBox.classList.remove("dragging");
+//     autoScroll();
+// }
 
-const dragStop = () => {
-    isDragging = false;
-    tabsBox.classList.remove("dragging");
-    autoScroll();
-}
+// function autoScroll()
+// {
+//     let maxScrollLeft = tabsBox.scrollWidth - tabsBox.clientWidth;
+//     if(tabsBox.scrollLeft >= maxScrollLeft) slideGap = -30;
+//     else if(tabsBox.scrollLeft <= 0) slideGap = 30;
+//     if(isDragging == false && isHover == false) 
+//     {
+//         tabsBox.scrollBy({
+//             left: slideGap,
+//             behavior: "smooth"
+//         });
+//     }
+//     setTimeout(autoScroll, 1500);
+// }
+// const mouseLeave = () => {
+//     isHover = false;
+//     autoScroll();
+// }
 
-function autoScroll()
-{
-    let maxScrollLeft = tabsBox.scrollWidth - tabsBox.clientWidth;
-    if(tabsBox.scrollLeft >= maxScrollLeft) slideGap = -30;
-    else if(tabsBox.scrollLeft <= 0) slideGap = 30;
-    if(isDragging == false && isHover == false) 
-    {
-        tabsBox.scrollBy({
-            left: slideGap,
-            behavior: "smooth"
-        });
-    }
-    setTimeout(autoScroll, 1500);
-}
-const mouseLeave = () => {
-    isHover = false;
-    autoScroll();
-}
-
-autoScroll();
-tabsBox.addEventListener("mousedown", () => isDragging = true);
-tabsBox.addEventListener("mousemove", dragging);
-document.addEventListener("mouseup", dragStop);
-tabsBox.addEventListener("touchmove", () => isDragging = true);
-tabsBox.addEventListener("touchend", () => dragStop());
+// autoScroll();
+// tabsBox.addEventListener("mousedown", () => isDragging = true);
+// tabsBox.addEventListener("mousemove", dragging);
+// document.addEventListener("mouseup", dragStop);
+// tabsBox.addEventListener("touchmove", () => isDragging = true);
+// tabsBox.addEventListener("touchend", () => dragStop());
 
 
-let slides = ["../public/slider/parking.jpg", "../public/slider/airport.jpg", "../public/slider/cctv.jpg"]
+let slides = ["../public/airportw.jpg", "../public/airportw.jpg", "../public/airportw.jpg"]
 let slidesDesc = ["Slika našeg parkinga", "Sarajevski aerodrom", "Naša sigurnosna zaštita"];
 let slideCounter = 0;
 
-document.getElementById("right_arrow").addEventListener("click", () => {
+document.getElementById("arrow_right").addEventListener("click", () => {
     slideCounter++;
     if(slideCounter > 2) slideCounter = 0;
     slideChange();
 })
-document.getElementById("left_arrow").addEventListener("click", () => {
+document.getElementById("arrow_left").addEventListener("click", () => {
     slideCounter--;
     if(slideCounter < 0) slideCounter = 2;
     slideChange();
@@ -137,10 +140,10 @@ const changeImage = () => {
     slideImage.style.backgroundImage = `url(${slides[slideCounter]})`;
 }
 
-const navBar = document.querySelector(".navbar_ul");
-const ham = document.getElementById("ham");
+// const navBar = document.querySelector(".navbar_ul");
+// const ham = document.getElementById("ham");
 
-ham.addEventListener("click", () => {
-    navBar.classList.toggle("active");
-})
+// ham.addEventListener("click", () => {
+//     navBar.classList.toggle("active");
+// })
 
